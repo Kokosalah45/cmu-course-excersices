a2dea1a2afeb1ff17e77b06db8f3eb1642742a3f
Kerolous Amged <kerolous.amged@nagwa.com>
2024-05-01 08:23:54 +0300

    update homework with the 5th solution

--- 1-homework/5.sql
+++ 1-homework/5.sql
@@ -1,10 +1,34 @@
-select a.name ,  ri.date_month , count(a.name) as number_of_releases_each_month
-from artist a
-join artist_type at on at.id = a.type
-join artist_credit_name acn on acn.artist = a.id
-join artist_credit ac on ac.id = acn.artist_credit
-join release r on r.artist_credit = ac.id
-join release_info ri on ri.release = r.id
-where a.name like 'Elvis%' and at.name = 'Person' and ri.date_month is not null
-group by a.name , ri.date_month
-order by a.name , ri.date_month , number_of_releases_each_month desc;
+with ranked_months as (
+    select 
+        a.name,
+        ri.date_month,
+        count(*) as number_of_releases,
+        dense_rank() over (partition by a.name order by count(*) desc, ri.date_month) as rank
+    from 
+        artist a
+        join artist_type at on at.id = a.type
+        join artist_credit_name acn on acn.artist = a.id
+        join artist_credit ac on ac.id = acn.artist_credit
+        join release r on r.artist_credit = ac.id
+        join release_info ri on ri.release = r.id
+    where 
+        a.name like 'Elvis%'
+        and at.name = 'Person'
+        and ri.date_month is not null
+    group by 
+        a.name, ri.date_month
+)
+
+
+
+select 
+    name,
+    date_month as month_with_most_releases,
+    number_of_releases
+from 
+    ranked_months
+where 
+    rank = 1
+order by 
+    number_of_releases desc,
+    name;
